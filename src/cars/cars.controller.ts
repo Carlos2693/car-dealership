8b37476705d464abd6d9de6f162aa4dc78ea8487
Carlos Buruel <charly.ob.26@gmail.com>
2024-07-15 19:15:57 -0600

    37. Inyección de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    
+    constructor(
+        private readonly cardsService: CarsService
+    ) {}
 
     @Get()
     getAllCards() {
-        return this.cars
+        return this.cardsService.findAll()
     }
 
     @Get(':id')
     getCarById(@Param('id') id: string) {
-        return this.cars[id]
+        return this.cardsService.findOnById(+id)
     }
 }
